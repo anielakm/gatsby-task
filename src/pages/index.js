8bcff76c0a0f2526f1e147da92b9cceb7b97769b
anielakm <a.katanamatlok@gmail.com>
2019-10-04 10:12:37 +0200

    added global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,21 @@
 import React from "react"
+import { createGlobalStyle } from "styled-components";
+const Global = createGlobalStyle`
+
+body{
+  padding:0;
+  margin:0;
+  font-family: 'Lato', serif;
+ 
+}
+*, *::before, *::after{
+  box-sizing:border-box;
+}
+`;
 
 const IndexPage = () => (
   <>
+    <Global />
     index
   </>
 )
